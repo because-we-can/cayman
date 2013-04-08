cec8a8d76f296d028524c9c9bbb894c364d6642f
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2013-04-08 23:59:34 +0200

    Adding first iteration of #1 (WIP)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
 angular.module('caymanApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+      $scope.accounts = JSON.parse(localStorage.getItem('cayman.accounts'));
+
+      $scope.addAccount = function (account) {
+        $scope.accounts.push(account);
+      };
+
+      $scope.removeAccount = function (account) {
+        $scope.accounts.push(account);
+      };
+
+      $scope.$watch('accounts', function (newItems, oldItems) {
+        localStorage.setItem('cayman.accounts', JSON.stringify($scope.accounts));
+      }, true);
+    });
