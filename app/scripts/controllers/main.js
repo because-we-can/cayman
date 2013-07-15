4cb1c6afcf0fdf3cacdba952e331dc2adf2f6d80
tristanles <tristanlescut@gmail.com>
2013-07-15 18:17:00 -0400

    Application skeleton made with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('caymanApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
