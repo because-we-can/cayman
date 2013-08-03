751fdf0d69ba30f5b670a3eb78504cc2dacd1ed0
tristanles <tristanlescut@gmail.com>
2013-08-03 16:09:06 -0400

    Home page now displays the number of accounts and budgets

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -15,10 +15,18 @@ describe('Cayman App', function () {
       expect(repeater('.accounts li', 'accounts list').row(1)).toEqual(['Savings']);
     });
 
+    it('should display the number of accounts', function() {
+      expect(element('.accounts h3', 'accounts title').text()).toBe('2 account(s)');
+    });
+
     it('should display the budgets list', function () {
       expect(repeater('.budgets li', 'budgets list').count()).toBe(2);
       expect(repeater('.budgets li', 'budgets list').row(0)).toEqual(['Vacations']);
       expect(repeater('.budgets li', 'budgets list').row(1)).toEqual(['New laptop']);
+    });
+
+    it('should display the number of budgets', function() {
+      expect(element('.budgets h3', 'budgets title').text()).toBe('2 budget(s)');
     });
   });
 });
