12244eccae7fec4fd2a862ba6d8c5ac0b9220219
tristanles <tristanlescut@gmail.com>
2013-08-03 17:49:59 -0400

    Added unit tests for adding accounts and budgets

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,8 +5,10 @@ describe('MainCtrl tests', function () {
   // load the controller's module
   beforeEach(module('caymanApp'));
 
-  var MainCtrl,
-      scope;
+  var MainCtrl;
+  var scope;
+  var ACCOUNT_NAME = 'A test account';
+  var BUDGET_NAME = "A budget name";
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
@@ -32,5 +34,19 @@ describe('MainCtrl tests', function () {
     expect(budgets.length).toBe(2);
     expect(budgets[0]).toBe('Vacations');
     expect(budgets[1]).toBe('New laptop');
+  });
+
+  it('adds a new account to the scope', function() {
+    scope.addAccount(ACCOUNT_NAME);
+
+    expect(scope.accounts.length).toBe(3);
+    expect(scope.accounts[2]).toBe(ACCOUNT_NAME);
+  });
+
+  it('adds a new budget to the scope', function() {
+    scope.addBudget(BUDGET_NAME);
+
+    expect(scope.budgets.length).toBe(3);
+    expect(scope.budgets[2]).toBe(BUDGET_NAME);
   });
 });
