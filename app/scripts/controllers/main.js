f2b57cac5fa9772e0714c15f8067dcbf5c4717ca
tristanles <tristanlescut@gmail.com>
2013-08-03 17:43:36 -0400

    First mockup for adding accounts and budgets

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,4 +4,22 @@ angular.module('caymanApp')
     .controller('MainCtrl', function ($scope) {
       $scope.accounts = ['Check', 'Savings'];
       $scope.budgets = ['Vacations', 'New laptop'];
+      $scope.newAccountName = '';
+      $scope.newBudgetName = '';
+
+      $scope.addAccount = function(newAccountName) {
+        if(!newAccountName) {
+          return;
+        }
+        $scope.accounts.push(newAccountName);
+        $scope.newAccountName = '';
+      };
+
+      $scope.addBudget = function(newBudgetName) {
+        if(!newBudgetName) {
+          return;
+        }
+        $scope.budgets.push(newBudgetName);
+        $scope.newBudgetName = ''
+      }
     });
