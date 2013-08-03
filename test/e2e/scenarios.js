1421ef9bb8b6a8c09b54d9546b359b18cb4bde70
tristanles <tristanlescut@gmail.com>
2013-08-03 18:18:10 -0400

    Added e2e tests for adding budgets

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -9,24 +9,49 @@ describe('Cayman App', function () {
     });
 
 
-    it('should display the accounts list', function () {
-      expect(repeater('.accounts li', 'accounts list').count()).toBe(2);
+    it('displays the accounts list and a form', function () {
+      expect(repeater('.accounts li', 'accounts list').count()).toBe(3);
       expect(repeater('.accounts li', 'accounts list').row(0)).toEqual(['Check']);
       expect(repeater('.accounts li', 'accounts list').row(1)).toEqual(['Savings']);
-    });
 
-    it('should display the number of accounts', function() {
+      expect(element('.accounts li input', 'new accounts input').html()).toBeDefined();
+      expect(element('.accounts li button', 'new accounts button').html()).toBeDefined();
+
       expect(element('.accounts h3', 'accounts title').text()).toBe('2 account(s)');
     });
 
-    it('should display the budgets list', function () {
-      expect(repeater('.budgets li', 'budgets list').count()).toBe(2);
-      expect(repeater('.budgets li', 'budgets list').row(0)).toEqual(['Vacations']);
-      expect(repeater('.budgets li', 'budgets list').row(1)).toEqual(['New laptop']);
+    it('adds a new account', function() {
+      var newAccount = 'A new account';
+
+      input('newAccountName').enter(newAccount);
+      element('.accounts li:nth-last-child(1) button').click();
+
+      expect(repeater('.accounts li', 'accounts list').count()).toBe(4);
+      expect(repeater('.accounts li', 'accounts list').row(2)).toEqual([newAccount]);
+      expect(element('.accounts h3', 'accounts title').text()).toBe('3 account(s)');
     });
 
-    it('should display the number of budgets', function() {
+    it('displays the budgets list and a form', function () {
+      expect(repeater('.budgets li', 'budgets list').count()).toBe(3);
+      expect(repeater('.budgets li', 'budgets list').row(0)).toEqual(['Vacations']);
+      expect(repeater('.budgets li', 'budgets list').row(1)).toEqual(['New laptop']);
+
+      expect(element('.budgets li input', 'new budgets input').html()).toBeDefined();
+      expect(element('.budgets li button', 'new budgets button').html()).toBeDefined();
+
       expect(element('.budgets h3', 'budgets title').text()).toBe('2 budget(s)');
     });
+
+    it('adds a new budget', function() {
+      var newBudget = 'A new budget';
+
+      input('newBudgetName').enter(newBudget);
+      element('.budgets li:nth-last-child(1) button').click();
+
+      expect(repeater('.budgets li', 'budgets list').count()).toBe(4);
+      expect(repeater('.budgets li', 'budgets list').row(2)).toEqual([newBudget]);
+      expect(element('.budgets h3', 'budgets title').text()).toBe('3 budget(s)');
+    });
+
   });
 });
